d387f9dd90b2a8d2b2fed299fde760e4eb5aeb44
david ornelas <davidornelas_11@hotmail.com>
2021-02-25 13:08:14 -0700

    created react app

---
+++ src/data.js
@@ -0,0 +1,87 @@
+export const userData = [
+  {
+    createdAt: "January 28 2021 at 12:54:29 PM UTC-7",
+    defaultListingId: "2K6OjdqN0",
+    displayName: "Michael Dudgeon",
+    listings: [
+      {
+        id: "2K6OjdqN0",
+        role: "owner",
+      },
+    ],
+    number: "+15033333333",
+    pendingListingIds: "2K6OjdqN0",
+    photoURL:
+      "https://lh6.googleusercontent.com/-x2bPbVsp8SI/AAAAAAAAAAI/AAAAAAAAAAA/AMZuucmrF-_DZO4VrZRtIRfrMUz60YfBvw/s96-c/photo.jpg",
+  },
+  {
+    createdAt: "January 5, 2021 at 4:33:07 PM UTC-7",
+    displayName: "Andy joslin",
+    number: "+14803998538",
+  },
+  {
+    createdAt: "January 30, 2021 at 12:58:26 PM UTC-7",
+    defaultListingId: "ON1gk8p1I",
+    displayName: "Nicholas Massie",
+    listings: [
+      {
+        id: "ON1gk8p1I",
+        role: "owner",
+      },
+    ],
+    number: "+12702312537",
+    pendingListingId: "ON1gk8p1I",
+    photoURL:
+      "https://lh4.googleusercontent.com/-XHC55bB5zZ4/AAAAAAAAAAI/AAAAAAAAAAA/AMZuucn08wGR_uPJ883HHAKoc1tzCE0GYg/s96-c/photo.jpg",
+    verification: {
+      amount: "555500000",
+      documentURL:
+        "https://firebasestorage.googleapis.com/v0/b/finding-spaces-73b23.appspot.com/o/users%2FixBRvKwFA9TZTX7qeiZ7Fos9hjX2%2Fsql-cheat-sheet.pdf?alt=media&token=ab9371da-370b-403d-afaa-9de6d90e939f",
+      lender: "20000",
+      loanType: "200000",
+      status: "pending",
+      verifType: "preQualification",
+    },
+  },
+];
+
+export const listingData = [
+  {
+    activity: "Just Listed!",
+    createdAt: "January 30, 2021 at 12:59:33 PM UTC-7",
+    fullAddress: "27023 Storrie Lake Dr, Moreno Valley, CA 92555, USA",
+    id: "ON1gk8p1I",
+    interest_count: 0,
+    primaryOwnerId: "ixBRvKwFA9TZTX7qeiZ7Fos9hjX2",
+    showingCount: "0",
+    signCode: {
+      checkoutOut: false,
+      formatted: "GY-731",
+      id: "GY731",
+      letters: "GY",
+      number: 731,
+      signCode: "GY731",
+    },
+    uniqueViews: "0",
+  },
+  {
+    activity: "Just Listed!",
+    createdAt: "January 30, 2021 at 6:54:01 AM UTC-7",
+    fullAddress: "1330 S 16th St, Phoenix, AZ 85034, USA",
+    id: "2K6OjdqN0",
+    interest_count: 0,
+    primaryOwnerId: "MjVHw5jEsAbPkklUGNWh6GntVy32",
+    showingCount: "0",
+    signCode: {
+      checkedOut: false,
+      formatted: "OJ-392",
+      id: "OJ392",
+      letters: "OJ",
+      number: 392,
+      signCode: "OJ392",
+    },
+    uniqueViews: "0",
+  },
+];
+
+
